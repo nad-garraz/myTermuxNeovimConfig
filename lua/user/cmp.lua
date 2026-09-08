b589d6aec699f213bb41ce16fbd1d45d5044c9e1
nad-garraz <22796744+nad-garraz@users.noreply.github.com>
2026-09-08 10:40:00 -0300

    cmp neogit

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -1,90 +1,96 @@
 local M = {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
+  'hrsh7th/nvim-cmp',
   dependencies = {
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-emoji',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'saadparwaiz1/cmp_luasnip',
     {
-      "hrsh7th/cmp-nvim-lsp",
-      event = "InsertEnter",
-    },
-    {
-      "hrsh7th/cmp-emoji",
-      event = "InsertEnter",
-    },
-    {
-      "hrsh7th/cmp-buffer",
-      event = "InsertEnter",
-    },
-    {
-      "hrsh7th/cmp-path",
-      event = "InsertEnter",
-    },
-    {
-      "hrsh7th/cmp-cmdline",
-      event = "InsertEnter",
-    },
-    {
-      "saadparwaiz1/cmp_luasnip",
-      event = "InsertEnter",
-    },
-    {
-      "L3MON4D3/LuaSnip",
-      event = "InsertEnter",
+      'L3MON4D3/LuaSnip',
       dependencies = {
-        "rafamadriz/friendly-snippets",
+        'rafamadriz/friendly-snippets',
       },
     },
-    {
-      "hrsh7th/cmp-nvim-lua",
-    },
+    'hrsh7th/cmp-nvim-lua',
   },
 }
 
 function M.config()
-  local cmp = require "cmp"
-  local luasnip = require "luasnip"
-  require("luasnip/loaders/from_vscode").lazy_load()
+  local cmp = require 'cmp'
+  local luasnip = require 'luasnip'
+  require('luasnip/loaders/from_vscode').lazy_load()
 
-  vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
-  vim.api.nvim_set_hl(0, "CmpItemKindTabnine", { fg = "#CA42F0" })
-  vim.api.nvim_set_hl(0, "CmpItemKindEmoji", { fg = "#FDE030" })
-
-  local check_backspace = function()
-    local col = vim.fn.col "." - 1
-    return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
-  end
-
-  local icons = require "user.icons"
+  local icons = require 'user.icons'
 
   cmp.setup {
     snippet = {
       expand = function(args)
-        luasnip.lsp_expand(args.body) -- For `luasnip` users.
+        luasnip.lsp_expand(args.body)
       end,
     },
     mapping = cmp.mapping.preset.insert {
-      ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-      ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-      ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-      ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-      ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-      ["<C-e>"] = cmp.mapping {
+      ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
+      ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-1), { 'i', 'c' }),
+      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(1), { 'i', 'c' }),
+      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+      ['<C-e>'] = cmp.mapping {
         i = cmp.mapping.abort(),
         c = cmp.mapping.close(),
       },
-      -- Accept currently selected item. If none selected, `select` first item.
-      -- Set `select` to `false` to only confirm explicitly selected items.
-      ["<C-y>"] = cmp.mapping.confirm { select = true },
+      ['<C-y>'] = cmp.mapping.confirm { select = true },
+    },
+    formatting = {
+      fields = { 'kind', 'abbr', 'menu' },
+      format = function(entry, vim_item)
+        if icons and icons.kind then
+          vim_item.kind = string.format('%s %s', icons.kind[vim_item.kind] or '', vim_item.kind)
+        end
+        vim_item.menu = ({
+          nvim_lsp = '[LSP]',
+          luasnip  = '[Snippet]',
+          buffer   = '[Buffer]',
+          path     = '[Path]',
+          nvim_lua = '[Lua]',
+        })[entry.source.name]
+        return vim_item
+      end,
     },
     sources = {
-      { name = "copilot" },
-      { name = "nvim_lsp" },
-      { name = "luasnip" },
-      { name = "cmp_tabnine" },
-      { name = "nvim_lua" },
-      { name = "buffer" },
-      { name = "path" },
-      { name = "calc" },
-      { name = "emoji" },
+      { name = 'nvim_lsp', priority = 1000 },
+      { name = 'luasnip', priority = 750, keyword_length = 2 },
+      { name = 'nvim_lua', priority = 500, keyword_length = 2 },
+      {
+        name = 'buffer',
+        priority = 250,
+        keyword_length = 2,
+        option = {
+          get_bufnrs = function()
+            local bufs = {}
+            for _, win in ipairs(vim.api.nvim_list_wins()) do
+              bufs[vim.api.nvim_win_get_buf(win)] = true
+            end
+            return vim.tbl_keys(bufs)
+          end,
+        },
+      },
+      { name = 'path', priority = 250, keyword_length = 3 },
+      { name = 'emoji', priority = 100, keyword_length = 2 },
+    },
+    sorting = {
+      priority_weight = 2,
+      comparators = {
+        cmp.config.compare.offset,
+        cmp.config.compare.exact,
+        cmp.config.compare.score,
+        cmp.config.compare.recently_used,
+        cmp.config.compare.locality,
+        cmp.config.compare.kind,
+        cmp.config.compare.sort_text,
+        cmp.config.compare.length,
+        cmp.config.compare.order,
+      },
     },
     confirm_opts = {
       behavior = cmp.ConfirmBehavior.Replace,
@@ -92,11 +98,11 @@ function M.config()
     },
     window = {
       completion = {
-        border = "rounded",
+        border = 'rounded',
         scrollbar = false,
       },
       documentation = {
-        border = "rounded",
+        border = 'rounded',
       },
     },
     experimental = {
